74f80853fa272cbce6c7e77f0103059ddc5c18c0
João DIvino <joaodivinod@gmail.com>
2020-02-13 07:30:42 -0300

    get and set criados

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,34 @@
 class CalcController{
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalCel = document.querySelector("#display");
+        let dataEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        dis
+
+    }
+
+    get _displayCalc(){
+        return this._displayCalc;
+    }
     
+    set _displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+        this.currentDate = valor;
+    }
+
+
 }
